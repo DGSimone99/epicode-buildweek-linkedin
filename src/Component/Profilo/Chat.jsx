c9d47bc538e54ca936002b47393f9ebdfb24d03a
Luca Zuin <zuin.luca1111@gmail.com>
2025-02-24 16:44:52 +0100

    chat collapse

--- src/Component/Profilo/Chat.jsx
+++ src/Component/Profilo/Chat.jsx
@@ -1,23 +1,31 @@
-import { Dropdown } from "react-bootstrap";
-import { PersonCircle, ThreeDots, PencilSquare } from "react-bootstrap-icons";
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
+import { PencilSquare, PersonCircle, ThreeDots } from "react-bootstrap-icons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Chat = () => {
-  return (
-    <div className="position-fixed bottom-0 end-0 m-3 border rounded-3 bg-white">
-      <Dropdown drop="up">
-        <Dropdown.Toggle variant="white">
-          <PersonCircle size={20} color="grey" />
-          Messaggistica
-          <ThreeDots size={20} color="grey" />
-          <PencilSquare size={20} color="grey" />
-        </Dropdown.Toggle>
+  const [open, setOpen] = useState(false);
 
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Messaggio 1</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Messaggio 2</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Messaggio 3</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+  return (
+    <div className="position-fixed bottom-0 end-0 bg-white border rounded-top" style={{ width: "275px" }}>
+      <Button onClick={() => setOpen(!open)} aria-controls="chat-collapse-content" aria-expanded={open} variant="light" className="w-100 p-0 border-0">
+        <div className="d-flex align-items-center justify-content-between p-2">
+          <PersonCircle size={20} color="grey" />
+          <span>Messaggistica</span>
+          <div className="d-flex gap-2 ms-auto">
+            <ThreeDots size={20} color="grey" />
+            <PencilSquare size={20} color="grey" />
+          </div>
+        </div>
+      </Button>
+      <Collapse in={open}>
+        <div className="p-2" id="chat-collapse-content">
+          <h4>Ancora nessun messaggio</h4>
+          <p>Entra in contatto e dai il via a una conversazione per far decollare la tua carriera</p>
+          <Button variant="white">Invia un messaggio</Button>
+        </div>
+      </Collapse>
     </div>
   );
 };
