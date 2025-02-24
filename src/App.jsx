a368b3e82631cc7cf166f0b28e82f3c87b770dee
lorella-marino <lorellamarino@gmail.com>
2025-02-24 13:31:53 +0100

    commit 13:31

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale";
+import CustomNav from "./Component/CustomNav";
 
 function App() {
   return (
     <>
+      <CustomNav />
       <ProfiloPrincipale />
     </>
   );
