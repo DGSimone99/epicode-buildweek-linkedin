6eb0d2fe70d0985a3d84e9f1fe4a3c2df1c03942
lorella-marino <lorellamarino@gmail.com>
2025-02-24 15:05:48 +0100

    commit 15:05

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Analisi from "./Component/Profilo/Analisi";
 import Consigliato from "./Component/Profilo/Consigliato";
-/* import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale"; */
 import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale";
 import Language from "./Component/Profilo/Language";
 import Chat from "./Component/Profilo/Chat";
@@ -15,7 +14,6 @@ function App() {
     <>
       <CustomNav />
       <Container fluid>
-<<<<<<< HEAD
         <Row xs={1} sm={2}>
           <Col sm={8}>
             <ProfiloPrincipale />
@@ -27,21 +25,6 @@ function App() {
             <Persone />
           </Col>
         </Row>
-=======
-        <Container>
-          <Row>
-            <Col xs={8}>
-              <ProfiloPrincipale />
-              <Consigliato />
-              <Analisi />
-            </Col>
-            <Col xs={4}>
-              <Language />
-              <Persone />
-            </Col>
-          </Row>
-        </Container>
->>>>>>> 32aa37ddd0dbc4b54c581fe9e328d3b87c7e2077
       </Container>
       <Chat />
     </>
