0df365e9fae9e4ed636cdabf6c0eac1466569481
DGSimone99 <simone.digiorgio99@gmail.com>
2025-02-24 13:32:10 +0100

    Scheda Profilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale";
 
 function App() {
---
+++ src/Component/Profilo/Team.jsx
@@ -0,0 +1,116 @@
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import { useState } from "react";
+import { BsFillCameraFill, BsXLg } from "react-icons/bs";
+import { BiCamera, BiPencil } from "react-icons/bi";
+
+const Team = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Container>
+      <Card className="cardBox rounded-3">
+        <Card.Img
+          variant="top"
+          src="https://www.reliant-rehab.com/wp-content/uploads/2019/02/reliant-generic-background-1.jpg"
+          className="imgTeam position-relative"
+        />
+        <Button className="position-absolute cameraDiv bg-white rounded-circle p-2">
+          <BsFillCameraFill className="camera"></BsFillCameraFill>
+        </Button>
+        <Image
+          src="https://i.pinimg.com/736x/f3/d1/ed/f3d1edf10d63c40e1fa06364176fa502.jpg"
+          className="userImg rounded-circle"
+        ></Image>
+        <Card.Body className="mt-5 px-4 position-relative">
+          <BiPencil className="position-absolute pencil"></BiPencil>
+          <Row>
+            <Col xs={8}>
+              <Card.Title className="fs-2 mb-0">Team 3 Epicode</Card.Title>
+              <Card.Text className="fs-5">Studente presso EPICODE</Card.Text>
+              <Card.Text className="text-secondary">
+                Roma, Lazio, Italia <a href="#">Informazioni di contatto</a>
+              </Card.Text>
+            </Col>
+            <Col>
+              <img
+                src="https://media.licdn.com/dms/image/v2/C4E0BAQHYgix-Ynux1A/company-logo_100_100/company-logo_100_100/0/1646830188798/epicodeschool_logo?e=1748476800&amp;v=beta&amp;t=doTFJx0D6s9OjWg9ZXuLy9TSWloZEUtDzU0J_gPyBJs"
+                width={32}
+              ></img>
+              EPICODE
+            </Col>
+          </Row>
+          <div className="mt-4">
+            <Button variant="primary" className="rounded-pill fw-bold">
+              Disponibile per
+            </Button>
+            <Button variant="outline-primary" className="rounded-pill fw-bold mx-2">
+              Aggiungi sezione del profilo
+            </Button>
+            <Button variant="outline-primary" className="rounded-pill fw-bold me-2">
+              Migliora profilo
+            </Button>
+            <Button variant="outline-secondary" className="rounded-pill fw-bold">
+              Risorse
+            </Button>
+          </div>
+          <Carousel activeIndex={index} onSelect={handleSelect} interval={null} className="mt-5">
+            <Carousel.Item>
+              <Row>
+                <Col>
+                  <div className="border border-1 px-3 py-2 rounded-3 position-relative">
+                    <p className="mb-0 pe-5">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio voluptates earum
+                      blanditiis amet, tempore.
+                    </p>
+                    <a href="#">Inizia</a>
+                    <BsXLg className="position-absolute xCarousel"></BsXLg>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="border border-1 px-3 py-2 rounded-3 position-relative">
+                    <p className="mb-0 pe-5">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio voluptates earum
+                      blanditiis amet, tempore.
+                    </p>
+                    <a href="#">Inizia</a>
+                    <BsXLg className="position-absolute xCarousel"></BsXLg>
+                  </div>
+                </Col>
+              </Row>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Row>
+                <Col>
+                  <div className="border border-1 px-3 py-2 rounded-3 position-relative">
+                    <p className="mb-0 pe-5">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio voluptates earum
+                      blanditiis amet, tempore.
+                    </p>
+                    <a href="#">Inizia</a>
+                    <BsXLg className="position-absolute xCarousel"></BsXLg>
+                  </div>
+                </Col>
+                <Col>
+                  <div className="border border-1 px-3 py-2 rounded-3 position-relative">
+                    <p className="mb-0 pe-5">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque distinctio voluptates earum
+                      blanditiis amet, tempore.
+                    </p>
+                    <a href="#">Inizia</a>
+                    <BsXLg className="position-absolute xCarousel"></BsXLg>
+                  </div>
+                </Col>
+              </Row>
+            </Carousel.Item>
+          </Carousel>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default Team;
