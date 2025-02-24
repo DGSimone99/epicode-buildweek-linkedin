e747ea965b0c77bd197c12ac0807c40dd078961d
DGSimone99 <simone.digiorgio99@gmail.com>
2025-02-24 15:04:48 +0100

    Profilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Analisi from "./Component/Profilo/Analisi";
-import Consigliato from "./Component/Profilo/Consigliato";
-/* import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale"; */
 import ProfiloPrincipale from "./Component/Profilo/ProfiloPrincipale";
-import Language from "./Component/Profilo/Language";
 import Chat from "./Component/Profilo/Chat";
-import Persone from "./Component/Profilo/Persone";
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import CustomNav from "./Component/CustomNav";
 
 function App() {
@@ -16,17 +11,7 @@ function App() {
       <CustomNav />
       <Container fluid>
         <Container>
-          <Row>
-            <Col xs={8}>
-              <ProfiloPrincipale />
-              <Consigliato />
-              <Analisi />
-            </Col>
-            <Col xs={4}>
-              <Language />
-              <Persone />
-            </Col>
-          </Row>
+          <ProfiloPrincipale />
         </Container>
       </Container>
       <Chat />
