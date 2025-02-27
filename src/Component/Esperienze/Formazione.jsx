566b8b0e51bfd6e425a5851d616bd916f64c4ff1
lorella-marino <lorellamarino@gmail.com>
2025-02-27 17:56:28 +0100

    commit 17:56

--- src/Component/Esperienze/Formazione.jsx
+++ src/Component/Esperienze/Formazione.jsx
@@ -1,6 +1,7 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import React from "react";
 import FormazioneModal from "./FormazioneModal";
+import { Bank2 } from "react-bootstrap-icons";
 
 const Formazione = () => {
   const [modalShow, setModalShow] = React.useState(false);
@@ -12,7 +13,21 @@ const Formazione = () => {
           Mostrando le tue qualifiche avrai fino a 2 volte più probabilità di ricevere un messaggio InMail da un
           recruiter
         </p>
-        <p>PLACEHOLDER PER FORMAZIONE</p>
+        <Row>
+          <Col xs={2} xl={1} className="d-flex">
+            <div
+              className="d-flex justify-content-center align-items-center p-3 border border-secondary rounded-3 me-auto"
+              style={{ width: "50px", height: "50px" }}
+            >
+              <Bank2 className="text-secondary" />
+            </div>
+          </Col>
+          <Col className="text-secondary">
+            <h3 className="fs-5">Scuola o università</h3>
+            <p>Laurea, campo di studi</p>
+            <p>2019-2023</p>
+          </Col>
+        </Row>
         <Button
           variant="light"
           className="rounded-pill border border-primary fw-bold d-flex align-items-center justify-content-center me-auto text-primary"
